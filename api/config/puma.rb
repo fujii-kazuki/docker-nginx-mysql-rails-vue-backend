137e4cbd4601b98d2011165b6e28fd314906a758
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-12-23 02:35:53 +0900

    [Add] pumaの設定ファイルにソケットを作成するコードを追加

--- api/config/puma.rb
+++ api/config/puma.rb
@@ -33,3 +33,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+bind "unix:///backend/api/tmp/sockets/puma.sock"
